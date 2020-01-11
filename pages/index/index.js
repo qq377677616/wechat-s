65cf5a01958d8c681fa73d7cf9efd64741ad8715
songziquan <377677616@qq.com>
2020-01-11 11:37:39 +0800

    szq20200111

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,37 +9,37 @@ Page({
   data: {
     contentType: 0,//页面内容类型0为小程序模板内容，1为h5游戏嵌入小程序壳子审核专用内容
     jumpList: [//模板列表
-      { name: "授权", url: "/pages/pages-list/authorize/authorize "},
-      { name: "获取手机号", url: "/pages/pages-list/get-phone/get-phone "},
-      { name: "嵌入h5", url: "/pages/pages-list/webview/webview "},
-      { name: "地理位置定位", url: "/pages/pages-list/location/location "},
-      { name: "生成海报", url: "/pages/pages-list/poster/poster "},
-      { name: "生成二维码", url: "/pages/pages-list/codes/codes "},
-      { name: "日历", url: "/pages/pages-list/calendar/index "},
-      { name: "html解析", url: "/pages/pages-list/html/html "},
-      { name: "摇一摇", url: "/pages/pages-list/shake/shake "},
-      { name: "环形进度条", url: "/pages/pages-list/circle/circle "},
-      { name: "charts图表", url: "/pages/pages-list/wx-charts/index/index "},
-      { name: "星星评分", url: "/pages/pages-list/star-score/star-score "},
-      { name: "抽奖系列", url: "/pages/pages-list/prize/index "},
-      { name: "地图", url: "/pages/pages-list/map/index "},
-      { name: "websocket", url: "/pages/pages-list/websocket/websocket "},
-      { name: "AR", url: "/pages/pages-list/ar/ar "},
-      { name: "图片验证码", url: "/pages/pages-list/img-code/img-code "},
-      { name: "计算属性与监听", url: "/pages/pages-list/computed-watch/computed-watch "},
-      { name: "中英文语言", url: "/pages/pages-list/ch-en/ch-en "},
-      { name: "序列帧", url: "/pages/pages-list/sequence/sequence "},
-      { name: "导航栏和tabBar", url: "/pages/pages-list/custom/custom "},
-      { name: "3d轮播", url: "/pages/pages-list/swiper3d/swiper3d "},
-      { name: "滚动淡出", url: "/pages/pages-list/scroll-show/scroll-show "},
-      { name: "电子手写签名", url: "/pages/pages-list/handwriting/handwriting "},
-      { name: "机器人", url: "/pages/pages-list/robot/robot "},
-      { name: "全景", url: "/pages/pages-list/panorama/panorama "},
-      { name: "图片裁剪", url: "/pages/pages-list/cropper/cropper "},
-      { name: "滑动删除", url: "/pages/pages-list/sliding-del/sliding-del "},
-      { name: "抛物线动画", url: "/pages/pages-list/parabola/parabola "},
-      // { name: "测试", url: "/pages/pages-list/test/test "},
-      { name: "下一版本见...", url: " "}
+      { name: "授权", url: "/pages/pages-list/authorize/authorize"},
+      { name: "获取手机号", url: "/pages/pages-list/get-phone/get-phone"},
+      { name: "嵌入h5", url: "/pages/pages-list/webview/webview"},
+      { name: "地理位置定位", url: "/pages/pages-list/location/location"},
+      { name: "生成海报", url: "/pages/pages-list/poster/poster"},
+      { name: "生成二维码", url: "/pages/pages-list/codes/codes"},
+      { name: "日历", url: "/pages/pages-list/calendar/index"},
+      { name: "html解析", url: "/pages/pages-list/html/html"},
+      { name: "摇一摇", url: "/pages/pages-list/shake/shake"},
+      { name: "环形进度条", url: "/pages/pages-list/circle/circle"},
+      { name: "charts图表", url: "/pages/pages-list/wx-charts/index/index"},
+      { name: "星星评分", url: "/pages/pages-list/star-score/star-score"},
+      { name: "抽奖系列", url: "/pages/pages-list/prize/index"},
+      { name: "地图", url: "/pages/pages-list/map/index"},
+      { name: "websocket", url: "/pages/pages-list/websocket/websocket"},
+      { name: "AR", url: "/pages/pages-list/ar/ar"},
+      { name: "图片验证码", url: "/pages/pages-list/img-code/img-code"},
+      { name: "计算属性与监听", url: "/pages/pages-list/computed-watch/computed-watch"},
+      { name: "中英文语言", url: "/pages/pages-list/ch-en/ch-en"},
+      { name: "序列帧", url: "/pages/pages-list/sequence/sequence"},
+      { name: "导航栏和tabBar", url: "/pages/pages-list/custom/custom"},
+      { name: "3d轮播", url: "/pages/pages-list/swiper3d/swiper3d"},
+      { name: "滚动淡出", url: "/pages/pages-list/scroll-show/scroll-show"},
+      { name: "电子手写签名", url: "/pages/pages-list/handwriting/handwriting"},
+      { name: "机器人", url: "/pages/pages-list/robot/robot"},
+      { name: "全景", url: "/pages/pages-list/panorama/panorama"},
+      { name: "图片裁剪", url: "/pages/pages-list/cropper/cropper"},
+      { name: "滑动删除", url: "/pages/pages-list/sliding-del/sliding-del"},
+      { name: "抛物线动画", url: "/pages/pages-list/parabola/parabola"},
+      // { name: "测试", url: "/pages/pages-list/test/test"},
+      { name: "下一版本见...", url: ""}
     ]
   },
   onLoad() {
@@ -54,7 +54,7 @@ Page({
   },
   //跳转页面
   jumps(e) {
-    if (this.data.jumpList[e.currentTarget.dataset.index].url) tool.jump_rel(this.data.jumpList[e.currentTarget.dataset.index].url)
+    if (this.data.jumpList[e.currentTarget.dataset.index].url) tool.jump_nav(this.data.jumpList[e.currentTarget.dataset.index].url)
   },
   onShareAppMessage() {}
 })
